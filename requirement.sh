31d3463ad91a96789c6e12dfb5b112508d080fb3
yonessgt <youness.merzak123@gmail.com>
2023-12-13 22:06:06 +0000

    adding the requirement bash file

---
+++ requirement.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Requirements
+
+sudo pip3 install SQLAlchemy
+sudo apt install mysql-server
+sudo pip install Flask==2.1.0
+sudo pip install Werkzeug==2.3.7
+sudo pip3 install flask_cors
